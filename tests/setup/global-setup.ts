f08cf320a40cf7fa1f3034b61bf458bcfe038bbd
Arturo Trejo <arturo.trejo@resolver.com>
2023-11-21 18:52:52 -0500

    Implement tracing on global-setup function.

--- tests/setup/global-setup.ts
+++ tests/setup/global-setup.ts
@@ -3,6 +3,7 @@ import LoginPage from '../ui/pages/login-page';
 import uiPages from '../utils/uiPages';
 
 async function globalSetup(config: FullConfig) {
+  const globalSetupTracePath = './test-results/global-setup-trace.zip';
   const user = process.env.USERNAME!;
   const password = process.env.PASSWORD!;
   const { baseURL, storageState } = config.projects[0].use;
@@ -10,11 +11,19 @@ async function globalSetup(config: FullConfig) {
   const page = await browser.newPage();
   const loginPage = new LoginPage(page);
 
-  await page.goto(baseURL+uiPages.login);
-  await loginPage.doLogin(user, password);
-  await loginPage.checkLoggedIn();
-  await page.context().storageState({ path: storageState as string });
-  await browser.close();
+  try {
+    await page.context().tracing.start({ screenshots: true, snapshots: true });
+    await page.goto(baseURL+uiPages.login);
+    await loginPage.doLogin(user, password);
+    await loginPage.checkLoggedIn();
+    await page.context().storageState({ path: storageState as string });
+    await page.context().tracing.stop({ path: globalSetupTracePath });
+    await browser.close();
+  } catch (error) {
+    await page.context().tracing.stop({ path: globalSetupTracePath });
+    await browser.close();
+    throw error;
+  }
 }
 
 export default globalSetup;
